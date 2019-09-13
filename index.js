9b5d1e3e95c8a175ddb2a332709295ae878d9c38
drex904 <drew.nollsch@gmail.com>
2019-09-13 12:08:36 +0200

    prepare for 1.0.2

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = function(width, height) {
-    return width * height;
-  };
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Hello World, welcome to Drew's personal webpage! \r\n https://www.linkedin.com/in/drewnollsch \r\n https://github.com/drex04");
+});
+
+const server = app.listen(8080, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log(`Example app listening at http://${host}:${port}`);
+});
